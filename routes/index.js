a669d330255e6d009f967ee4be392c0a87a18793
S560285 <S560285@nwmissouri.edu>
2023-03-31 11:15:33 -0500

    added pressme button

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Venkata Mallikharjun Rao pola' });
 });
 
 module.exports = router;
